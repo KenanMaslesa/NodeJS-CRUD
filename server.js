41d4bf7932c4c6905b88ee7688ec54ce4f88e0be
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-10-17 03:25:33 +0200

    cors

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const bodyParser= require('body-parser');
 //Conect DB
 connectDB();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
