0f25b202d29e221b286fbd369f2f66d378f7c75f
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-10-17 18:37:12 +0200

    changed routes

--- routes/videos.js
+++ routes/videos.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const videoController = require('../controllers/video')
 
 //api/videos
-router.get('/get-all-videos', videoController.getVideos);
-router.get('/get-video/:videoId', videoController.getVideoById);
+router.get('/', videoController.getVideos);
+router.get('/:videoId', videoController.getVideoById);
 router.get('/load-more/:page', videoController.loadMoreVideos);
 router.post('/add-video', videoController.addVideo);
 router.put('/edit/:videoId', videoController.editVideo);
